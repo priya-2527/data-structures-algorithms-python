c90d6b0f42329cb1c73a4c634937452fec72ee61
priya-2527 <88970945+priya-2527@users.noreply.github.com>
2022-05-03 17:18:22 +0530

    Update 3_odd_even_numbers.py

--- data_structures/2_Arrays/Solution/3_odd_even_numbers.py
+++ data_structures/2_Arrays/Solution/3_odd_even_numbers.py
@@ -7,3 +7,15 @@ for i in range(1, max):
         odd_numbers.append(i)
 
 print("Odd numbers: ", odd_numbers)
+
+
+
+
+another way
+
+num=int(input("enter a number"))
+list2=[]
+for i in range(1,num+1,2):
+    list2.append(i)
+list2
+    
